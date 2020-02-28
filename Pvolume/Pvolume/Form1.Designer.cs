7256aae2ef479c356971d3ed8675ece1b4c0738a
Everton Souza <evertonts@ciandt.com>
2020-02-28 20:42:04 -0300

    Primeiro exercicio

---
+++ Pvolume/Pvolume/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Pvolume
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.btCalcular = new System.Windows.Forms.Button();
+            this.txtRaio = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtVolume = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btFechar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ALTURA";
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(87, 6);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(100, 20);
+            this.txtAltura.TabIndex = 1;
+            // 
+            // btCalcular
+            // 
+            this.btCalcular.Location = new System.Drawing.Point(15, 68);
+            this.btCalcular.Name = "btCalcular";
+            this.btCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btCalcular.TabIndex = 2;
+            this.btCalcular.Text = "CALCULAR";
+            this.btCalcular.UseVisualStyleBackColor = true;
+            this.btCalcular.Click += new System.EventHandler(this.btCalcular_Click);
+            // 
+            // txtRaio
+            // 
+            this.txtRaio.Location = new System.Drawing.Point(87, 32);
+            this.txtRaio.Name = "txtRaio";
+            this.txtRaio.Size = new System.Drawing.Size(100, 20);
+            this.txtRaio.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "RAIO";
+            // 
+            // txtVolume
+            // 
+            this.txtVolume.Location = new System.Drawing.Point(283, 6);
+            this.txtVolume.Name = "txtVolume";
+            this.txtVolume.ReadOnly = true;
+            this.txtVolume.Size = new System.Drawing.Size(100, 20);
+            this.txtVolume.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(208, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "VOLUME";
+            // 
+            // btFechar
+            // 
+            this.btFechar.Location = new System.Drawing.Point(112, 68);
+            this.btFechar.Name = "btFechar";
+            this.btFechar.Size = new System.Drawing.Size(75, 23);
+            this.btFechar.TabIndex = 7;
+            this.btFechar.Text = "FECHAR";
+            this.btFechar.UseVisualStyleBackColor = true;
+            this.btFechar.Click += new System.EventHandler(this.btFechar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 109);
+            this.Controls.Add(this.btFechar);
+            this.Controls.Add(this.txtVolume);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtRaio);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btCalcular);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Volume do Cilindro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.Button btCalcular;
+        private System.Windows.Forms.TextBox txtRaio;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtVolume;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btFechar;
+    }
+}
+
---
+++ Pvolume/Pvolume/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pvolume
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btFechar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btCalcular_Click(object sender, EventArgs e)
+        {
+            double altura, raio;
+            if (double.TryParse(txtAltura.Text, out altura) &&
+                double.TryParse(txtRaio.Text, out raio))
+                    txtVolume.Text = calcularVolume(raio, altura).ToString("N2");
+            else
+                MessageBox.Show("Entradas Inválidas");
+        }
+
+        public double calcularVolume(double raio, double altura)
+        {
+            double result;
+            result = Math.PI * Math.Pow(raio,2) * altura;
+            return result;
+        }
+    }
+}
