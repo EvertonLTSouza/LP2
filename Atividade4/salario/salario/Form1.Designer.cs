63c3b0fd787437dff5e334d3e5e27eee0c823500
Everton Souza <everton.luis.16@gmail.com>
2020-05-17 15:41:21 -0300

    Finalizado exercicio 4

--- Atividade4/salario/salario/Form1.Designer.cs
+++ Atividade4/salario/salario/Form1.Designer.cs
@@ -32,27 +32,28 @@
             this.lblSalarioBruto = new System.Windows.Forms.Label();
             this.lblFilhos = new System.Windows.Forms.Label();
             this.gpbSexo = new System.Windows.Forms.GroupBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.rbF = new System.Windows.Forms.RadioButton();
             this.rbM = new System.Windows.Forms.RadioButton();
+            this.rbF = new System.Windows.Forms.RadioButton();
             this.gpbCasado = new System.Windows.Forms.GroupBox();
             this.cbCasado = new System.Windows.Forms.CheckBox();
             this.btnVerificar = new System.Windows.Forms.Button();
             this.gpbDados = new System.Windows.Forms.GroupBox();
-            this.lblINSS = new System.Windows.Forms.Label();
-            this.lblIRPF = new System.Windows.Forms.Label();
-            this.lblSalFam = new System.Windows.Forms.Label();
             this.lblSalLiq = new System.Windows.Forms.Label();
-            this.txtINSS = new System.Windows.Forms.TextBox();
-            this.txtIRPF = new System.Windows.Forms.TextBox();
-            this.txtSalFam = new System.Windows.Forms.TextBox();
-            this.txtSalLiq = new System.Windows.Forms.TextBox();
-            this.lblDescINSS = new System.Windows.Forms.Label();
-            this.txtDescINSS = new System.Windows.Forms.TextBox();
             this.lblDescIRPF = new System.Windows.Forms.Label();
-            this.txtDescIRPF = new System.Windows.Forms.TextBox();
+            this.lblSalFam = new System.Windows.Forms.Label();
+            this.lblDescINSS = new System.Windows.Forms.Label();
+            this.lblIRPF = new System.Windows.Forms.Label();
+            this.lblINSS = new System.Windows.Forms.Label();
+            this.txtNomeFunc = new System.Windows.Forms.MaskedTextBox();
+            this.txtSalBruto = new System.Windows.Forms.MaskedTextBox();
+            this.txtNumFilhos = new System.Windows.Forms.MaskedTextBox();
+            this.txtINSS = new System.Windows.Forms.MaskedTextBox();
+            this.txtIRPF = new System.Windows.Forms.MaskedTextBox();
+            this.txtSalLiq = new System.Windows.Forms.MaskedTextBox();
+            this.txtSalFam = new System.Windows.Forms.MaskedTextBox();
+            this.txtDescINSS = new System.Windows.Forms.MaskedTextBox();
+            this.txtDescIRPF = new System.Windows.Forms.MaskedTextBox();
+            this.lblDados = new System.Windows.Forms.Label();
             this.gpbSexo.SuspendLayout();
             this.gpbCasado.SuspendLayout();
             this.gpbDados.SuspendLayout();
@@ -96,26 +97,15 @@
             this.gpbSexo.TabStop = false;
             this.gpbSexo.Text = "Sexo";
             // 
-            // textBox1
+            // rbM
             // 
-            this.textBox1.Location = new System.Drawing.Point(176, 27);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(133, 20);
-            this.textBox1.TabIndex = 2;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(176, 55);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(133, 20);
-            this.textBox2.TabIndex = 2;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(176, 81);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(133, 20);
-            this.textBox3.TabIndex = 2;
+            this.rbM.AutoSize = true;
+            this.rbM.Location = new System.Drawing.Point(29, 54);
+            this.rbM.Name = "rbM";
+            this.rbM.Size = new System.Drawing.Size(34, 17);
+            this.rbM.TabIndex = 0;
+            this.rbM.Text = "M";
+            this.rbM.UseVisualStyleBackColor = true;
             // 
             // rbF
             // 
@@ -128,16 +118,6 @@
             this.rbF.TabStop = true;
             this.rbF.Text = "F";
             this.rbF.UseVisualStyleBackColor = true;
-            // 
-            // rbM
-            // 
-            this.rbM.AutoSize = true;
-            this.rbM.Location = new System.Drawing.Point(29, 54);
-            this.rbM.Name = "rbM";
-            this.rbM.Size = new System.Drawing.Size(34, 17);
-            this.rbM.TabIndex = 0;
-            this.rbM.Text = "M";
-            this.rbM.UseVisualStyleBackColor = true;
             // 
             // gpbCasado
             // 
@@ -160,17 +140,18 @@
             // 
             // btnVerificar
             // 
-            this.btnVerificar.Location = new System.Drawing.Point(176, 140);
+            this.btnVerificar.Location = new System.Drawing.Point(176, 116);
             this.btnVerificar.Name = "btnVerificar";
             this.btnVerificar.Size = new System.Drawing.Size(133, 23);
             this.btnVerificar.TabIndex = 3;
             this.btnVerificar.Text = "Verificar Descontos";
             this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
             // 
             // gpbDados
             // 
-            this.gpbDados.Controls.Add(this.txtSalLiq);
             this.gpbDados.Controls.Add(this.txtSalFam);
+            this.gpbDados.Controls.Add(this.txtSalLiq);
             this.gpbDados.Controls.Add(this.txtIRPF);
             this.gpbDados.Controls.Add(this.txtDescIRPF);
             this.gpbDados.Controls.Add(this.txtDescINSS);
@@ -188,33 +169,6 @@
             this.gpbDados.TabStop = false;
             this.gpbDados.Text = "Dados";
             // 
-            // lblINSS
-            // 
-            this.lblINSS.AutoSize = true;
-            this.lblINSS.Location = new System.Drawing.Point(6, 16);
-            this.lblINSS.Name = "lblINSS";
-            this.lblINSS.Size = new System.Drawing.Size(73, 13);
-            this.lblINSS.TabIndex = 1;
-            this.lblINSS.Text = "Aliquota INSS";
-            // 
-            // lblIRPF
-            // 
-            this.lblIRPF.AutoSize = true;
-            this.lblIRPF.Location = new System.Drawing.Point(6, 44);
-            this.lblIRPF.Name = "lblIRPF";
-            this.lblIRPF.Size = new System.Drawing.Size(72, 13);
-            this.lblIRPF.TabIndex = 1;
-            this.lblIRPF.Text = "Aliquota IRPF";
-            // 
-            // lblSalFam
-            // 
-            this.lblSalFam.AutoSize = true;
-            this.lblSalFam.Location = new System.Drawing.Point(6, 73);
-            this.lblSalFam.Name = "lblSalFam";
-            this.lblSalFam.Size = new System.Drawing.Size(76, 13);
-            this.lblSalFam.TabIndex = 1;
-            this.lblSalFam.Text = "Salário Família";
-            // 
             // lblSalLiq
             // 
             this.lblSalLiq.AutoSize = true;
@@ -223,50 +177,6 @@
             this.lblSalLiq.Size = new System.Drawing.Size(75, 13);
             this.lblSalLiq.TabIndex = 1;
             this.lblSalLiq.Text = "SalárioLíquido";
-            // 
-            // txtINSS
-            // 
-            this.txtINSS.Location = new System.Drawing.Point(102, 13);
-            this.txtINSS.Name = "txtINSS";
-            this.txtINSS.Size = new System.Drawing.Size(133, 20);
-            this.txtINSS.TabIndex = 3;
-            // 
-            // txtIRPF
-            // 
-            this.txtIRPF.Location = new System.Drawing.Point(102, 41);
-            this.txtIRPF.Name = "txtIRPF";
-            this.txtIRPF.Size = new System.Drawing.Size(133, 20);
-            this.txtIRPF.TabIndex = 3;
-            // 
-            // txtSalFam
-            // 
-            this.txtSalFam.Location = new System.Drawing.Point(102, 70);
-            this.txtSalFam.Name = "txtSalFam";
-            this.txtSalFam.Size = new System.Drawing.Size(133, 20);
-            this.txtSalFam.TabIndex = 3;
-            // 
-            // txtSalLiq
-            // 
-            this.txtSalLiq.Location = new System.Drawing.Point(102, 103);
-            this.txtSalLiq.Name = "txtSalLiq";
-            this.txtSalLiq.Size = new System.Drawing.Size(133, 20);
-            this.txtSalLiq.TabIndex = 3;
-            // 
-            // lblDescINSS
-            // 
-            this.lblDescINSS.AutoSize = true;
-            this.lblDescINSS.Location = new System.Drawing.Point(258, 16);
-            this.lblDescINSS.Name = "lblDescINSS";
-            this.lblDescINSS.Size = new System.Drawing.Size(81, 13);
-            this.lblDescINSS.TabIndex = 1;
-            this.lblDescINSS.Text = "Desconto INSS";
-            // 
-            // txtDescINSS
-            // 
-            this.txtDescINSS.Location = new System.Drawing.Point(354, 13);
-            this.txtDescINSS.Name = "txtDescINSS";
-            this.txtDescINSS.Size = new System.Drawing.Size(133, 20);
-            this.txtDescINSS.TabIndex = 3;
             // 
             // lblDescIRPF
             // 
@@ -277,22 +187,131 @@
             this.lblDescIRPF.TabIndex = 1;
             this.lblDescIRPF.Text = "Desconto IRPF";
             // 
+            // lblSalFam
+            // 
+            this.lblSalFam.AutoSize = true;
+            this.lblSalFam.Location = new System.Drawing.Point(6, 73);
+            this.lblSalFam.Name = "lblSalFam";
+            this.lblSalFam.Size = new System.Drawing.Size(76, 13);
+            this.lblSalFam.TabIndex = 1;
+            this.lblSalFam.Text = "Salário Família";
+            // 
+            // lblDescINSS
+            // 
+            this.lblDescINSS.AutoSize = true;
+            this.lblDescINSS.Location = new System.Drawing.Point(258, 16);
+            this.lblDescINSS.Name = "lblDescINSS";
+            this.lblDescINSS.Size = new System.Drawing.Size(81, 13);
+            this.lblDescINSS.TabIndex = 1;
+            this.lblDescINSS.Text = "Desconto INSS";
+            // 
+            // lblIRPF
+            // 
+            this.lblIRPF.AutoSize = true;
+            this.lblIRPF.Location = new System.Drawing.Point(6, 44);
+            this.lblIRPF.Name = "lblIRPF";
+            this.lblIRPF.Size = new System.Drawing.Size(72, 13);
+            this.lblIRPF.TabIndex = 1;
+            this.lblIRPF.Text = "Aliquota IRPF";
+            // 
+            // lblINSS
+            // 
+            this.lblINSS.AutoSize = true;
+            this.lblINSS.Location = new System.Drawing.Point(6, 16);
+            this.lblINSS.Name = "lblINSS";
+            this.lblINSS.Size = new System.Drawing.Size(73, 13);
+            this.lblINSS.TabIndex = 1;
+            this.lblINSS.Text = "Aliquota INSS";
+            // 
+            // txtNomeFunc
+            // 
+            this.txtNomeFunc.Location = new System.Drawing.Point(176, 27);
+            this.txtNomeFunc.Name = "txtNomeFunc";
+            this.txtNomeFunc.Size = new System.Drawing.Size(133, 20);
+            this.txtNomeFunc.TabIndex = 4;
+            // 
+            // txtSalBruto
+            // 
+            this.txtSalBruto.Location = new System.Drawing.Point(176, 55);
+            this.txtSalBruto.Mask = "00000000.00";
+            this.txtSalBruto.Name = "txtSalBruto";
+            this.txtSalBruto.Size = new System.Drawing.Size(133, 20);
+            this.txtSalBruto.TabIndex = 4;
+            // 
+            // txtNumFilhos
+            // 
+            this.txtNumFilhos.Location = new System.Drawing.Point(176, 81);
+            this.txtNumFilhos.Mask = "90";
+            this.txtNumFilhos.Name = "txtNumFilhos";
+            this.txtNumFilhos.Size = new System.Drawing.Size(133, 20);
+            this.txtNumFilhos.TabIndex = 4;
+            // 
+            // txtINSS
+            // 
+            this.txtINSS.Enabled = false;
+            this.txtINSS.Location = new System.Drawing.Point(102, 15);
+            this.txtINSS.Name = "txtINSS";
+            this.txtINSS.Size = new System.Drawing.Size(133, 20);
+            this.txtINSS.TabIndex = 4;
+            // 
+            // txtIRPF
+            // 
+            this.txtIRPF.Enabled = false;
+            this.txtIRPF.Location = new System.Drawing.Point(102, 41);
+            this.txtIRPF.Name = "txtIRPF";
+            this.txtIRPF.Size = new System.Drawing.Size(133, 20);
+            this.txtIRPF.TabIndex = 4;
+            // 
+            // txtSalLiq
+            // 
+            this.txtSalLiq.Enabled = false;
+            this.txtSalLiq.Location = new System.Drawing.Point(102, 103);
+            this.txtSalLiq.Name = "txtSalLiq";
+            this.txtSalLiq.Size = new System.Drawing.Size(133, 20);
+            this.txtSalLiq.TabIndex = 4;
+            // 
+            // txtSalFam
+            // 
+            this.txtSalFam.Enabled = false;
+            this.txtSalFam.Location = new System.Drawing.Point(102, 70);
+            this.txtSalFam.Name = "txtSalFam";
+            this.txtSalFam.Size = new System.Drawing.Size(133, 20);
+            this.txtSalFam.TabIndex = 4;
+            // 
+            // txtDescINSS
+            // 
+            this.txtDescINSS.Enabled = false;
+            this.txtDescINSS.Location = new System.Drawing.Point(354, 13);
+            this.txtDescINSS.Name = "txtDescINSS";
+            this.txtDescINSS.Size = new System.Drawing.Size(133, 20);
+            this.txtDescINSS.TabIndex = 4;
+            // 
             // txtDescIRPF
             // 
+            this.txtDescIRPF.Enabled = false;
             this.txtDescIRPF.Location = new System.Drawing.Point(354, 41);
             this.txtDescIRPF.Name = "txtDescIRPF";
             this.txtDescIRPF.Size = new System.Drawing.Size(133, 20);
-            this.txtDescIRPF.TabIndex = 3;
+            this.txtDescIRPF.TabIndex = 4;
+            // 
+            // lblDados
+            // 
+            this.lblDados.AutoSize = true;
+            this.lblDados.Location = new System.Drawing.Point(52, 144);
+            this.lblDados.Name = "lblDados";
+            this.lblDados.Size = new System.Drawing.Size(0, 13);
+            this.lblDados.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(582, 398);
+            this.Controls.Add(this.lblDados);
+            this.Controls.Add(this.txtNumFilhos);
+            this.Controls.Add(this.txtSalBruto);
+            this.Controls.Add(this.txtNomeFunc);
             this.Controls.Add(this.btnVerificar);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.gpbDados);
             this.Controls.Add(this.gpbCasado);
             this.Controls.Add(this.gpbSexo);
@@ -301,6 +320,7 @@
             this.Controls.Add(this.lblNomeFuncionario);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.gpbSexo.ResumeLayout(false);
             this.gpbSexo.PerformLayout();
             this.gpbCasado.ResumeLayout(false);
@@ -320,25 +340,26 @@
         private System.Windows.Forms.GroupBox gpbSexo;
         private System.Windows.Forms.RadioButton rbM;
         private System.Windows.Forms.RadioButton rbF;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.GroupBox gpbCasado;
         private System.Windows.Forms.CheckBox cbCasado;
         private System.Windows.Forms.Button btnVerificar;
         private System.Windows.Forms.GroupBox gpbDados;
-        private System.Windows.Forms.TextBox txtSalLiq;
-        private System.Windows.Forms.TextBox txtSalFam;
-        private System.Windows.Forms.TextBox txtIRPF;
-        private System.Windows.Forms.TextBox txtDescIRPF;
-        private System.Windows.Forms.TextBox txtDescINSS;
-        private System.Windows.Forms.TextBox txtINSS;
         private System.Windows.Forms.Label lblSalLiq;
         private System.Windows.Forms.Label lblDescIRPF;
         private System.Windows.Forms.Label lblSalFam;
         private System.Windows.Forms.Label lblDescINSS;
         private System.Windows.Forms.Label lblIRPF;
         private System.Windows.Forms.Label lblINSS;
+        private System.Windows.Forms.MaskedTextBox txtNomeFunc;
+        private System.Windows.Forms.MaskedTextBox txtSalBruto;
+        private System.Windows.Forms.MaskedTextBox txtSalFam;
+        private System.Windows.Forms.MaskedTextBox txtSalLiq;
+        private System.Windows.Forms.MaskedTextBox txtIRPF;
+        private System.Windows.Forms.MaskedTextBox txtDescIRPF;
+        private System.Windows.Forms.MaskedTextBox txtDescINSS;
+        private System.Windows.Forms.MaskedTextBox txtINSS;
+        private System.Windows.Forms.MaskedTextBox txtNumFilhos;
+        private System.Windows.Forms.Label lblDados;
     }
 }
 
